98f279cd068855daac2b0455c7e0ad3b3395484b
ChristGood <christ.good@yahoo.fr>
2017-06-27 17:27:01 +0200

    Mise en place du routing

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+//this compoenent handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './commons/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
